4cfafe32bd4a2b55600edf07a2bef9df6f55e129
MVP-Solutions-360 <maicol.londono@mvpsolutions.com>
2026-08-06 09:55:24 -0500

    Ajustes para crear el estatus historico

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from engine.pipeline import Pipeline
 
 Pipeline(
-    profile_path="profiles/wellezy/seguimientos_observaciones.json",
+    profile_path="profiles/wellezy/finance_accounts.json",
     input_file="input/excel/BD Cartera.xlsx"
 ).run()
